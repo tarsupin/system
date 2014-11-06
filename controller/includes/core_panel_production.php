7b78de3db30e23a11f5c098da4745fe0e37b0a7d
brint <brint@blendimc.com>
2014-11-05 23:03:03 -0600

    Removing MicroFaction Link from Core Panel

--- controller/includes/core_panel_production.php
+++ controller/includes/core_panel_production.php
@@ -10,7 +10,6 @@ echo str_replace("is-" . SITE_HANDLE, "is-" . SITE_HANDLE . " core-active", '
 			<li class="core-link is-social"><a href="http://unifaction.social/' . Me::$slg . '"><span class="icon-group core-icon"></span><span class="core-txt">Social</span></a></li>
 			<li class="core-link is-fastchat"><a href="http://fastchat.social/' . Me::$slg . '"><span class="icon-comments core-icon"></span><span class="core-txt">FastChat</span></a></li>
 			<li class="core-link is-hashtag"><a href="http://hashtag.unifaction.com/' . Me::$slg . '"><span class="icon-tag core-icon"></span><span class="core-txt">Hashtags</span></a></li>
-			<li class="core-link is-microfaction"><a href="http://community.microfaction.com/' . Me::$slg . '"><span class="icon-newspaper core-icon"></span><span class="core-txt">MicroFactions</span></a></li>
 			<li class="core-link is-blogfrog"><a href="http://blogfrog.social/' . Me::$slg . '"><span class="icon-pen core-icon"></span><span class="core-txt">BlogFrog</span></a></li>
 			<li class="core-link is-credits"><a href="http://unijoule.com/' . Me::$slg . '"><span class="icon-coin core-icon"></span><span class="core-txt">UniJoule</span></a></li>
 			
@@ -22,6 +21,7 @@ echo str_replace("is-" . SITE_HANDLE, "is-" . SITE_HANDLE . " core-active", '
 </div>');
 
 /*
+	//<li class="core-link is-microfaction"><a href="http://community.microfaction.com/' . Me::$slg . '"><span class="icon-newspaper core-icon"></span><span class="core-txt">MicroFactions</span></a></li>
 	//<li class="core-link is-my_uni"><a href="http://unifaction.me/' . Me::$slg . '"><span class="icon-star core-icon"></span><span class="core-txt">My Uni</span></a></li>
 	//<li class="core-link is-avatar"><a href="http://avatar.unifaction.com/' . Me::$slg . '"><span class="icon-user core-icon"></span><span class="core-txt">Avatar</span></a></li>
 	//<li class="core-link"><a href="http://unifaction.com/games' . Me::$slg . '"><span class="icon-gamepad core-icon"></span><span class="core-txt">Games</span></a></li>
